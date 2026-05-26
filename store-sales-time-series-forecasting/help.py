eff95d0e2bddf3c53504a15fc0a6c6007bf6e84a
dhanush-ballanki <dhanushballanki14@gmail.com>
2026-05-26 12:22:09 +0530

    quadratic regression

---
+++ store-sales-time-series-forecasting/help.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os 
+
+DATASETS_PATH = os.path.join('.', 'datasets')
+
+
+def fetch_data(filename):
+    filepath = os.path.join(DATASETS_PATH, filename)
+    data = pd.read_csv(filepath)
+    return data
+
+def create_submission_csv(pred, index):
+    submission = pd.DataFrame()
+    submission['Id'] = index
+    submission['sales'] = pred
+    submission.to_csv('submission.csv', index=False)
